becfd03b95296a0499d9a0fa623c86fc8d1cb806
DimitrK <jim.feedback@gmail.com>
2021-11-02 00:26:38 +0200

    implement needed contract logic for passing tests

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -13,29 +13,33 @@ contract SimpleBank {
 
     // Fill in the visibility keyword.
     // Hint: We want to protect our users balance from other contracts
-    mapping(address => uint256) balances;
+    mapping(address => uint256) private balances;
 
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping(address => bool) enrolled;
+    mapping(address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
-    address owner = msg.sender;
+    address public owner = msg.sender;
 
     /* Events - publicize actions to external listeners
      */
 
     // Add an argument for this event, an accountAddress
-    event LogEnrolled();
+    event LogEnrolled(address indexed accountAddress);
 
     // Add 2 arguments for this event, an accountAddress and an amount
-    event LogDepositMade();
+    event LogDepositMade(address indexed accountAddress, uint256 amount);
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance
-    event LogWithdrawal();
+    event LogWithdrawal(
+        address indexed accountAddress,
+        uint256 withdrawAmount,
+        uint256 newBalance
+    );
 
     modifier noReentrant() {
         require(!locked, "No re-entrancy");
@@ -65,22 +69,37 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint256) {
+    function getBalance() public view returns (uint256) {
         // 1. A SPECIAL KEYWORD prevents function from editing state variables;
         //    allows function to run locally/off blockchain
         // 2. Get the balance of the sender of this transaction
+        return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool) {
-        // 1. enroll of the sender of this transaction
+        require(enrolled[msg.sender] != true, "address already enrolled");
+
+        enrolled[msg.sender] = true;
+        emit LogEnrolled(msg.sender);
+        return true;
     }
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint256) {
+    function deposit()
+        public
+        payable
+        enrolledOnly(msg.sender)
+        returns (uint256)
+    {
+        require(msg.value > 0, "invalid deposit amount");
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+
+        return balances[msg.sender];
         // 1. Add the appropriate keyword so that this function can receive ether
         // 2. Users should be enrolled before they can make deposits
         // 3. Add the amount to the user's balance. Hint: the amount can be
@@ -93,7 +112,13 @@ contract SimpleBank {
     /// @dev This does not return any excess ether sent to it
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
-    function withdraw(uint256 withdrawAmount) public returns (uint256) {
+    function withdraw(uint256 withdrawAmount)
+        public
+        payable
+        enrolledOnly(msg.sender)
+        noReentrant
+        returns (uint256)
+    {
         // If the sender's balance is at least the amount they want to withdraw,
         // Subtract the amount from the sender's balance, and try to send that amount of ether
         // to the user attempting to withdraw.
@@ -102,5 +127,18 @@ contract SimpleBank {
         // 2. Transfer Eth to the sender and decrement the withdrawal amount from
         //    sender's balance
         // 3. Emit the appropriate event for this message
+        require(withdrawAmount > 0, "incorrect withdrawal amount");
+        require(balances[msg.sender] >= withdrawAmount, "insufficient funds");
+
+        uint256 newBalance =  balances[msg.sender] - withdrawAmount ;
+        balances[msg.sender] = newBalance;
+        (bool success, ) = msg.sender.call{value: withdrawAmount}("");
+
+        if (!success) {
+            revert("failed to send Eth");
+        }
+
+        emit LogWithdrawal(msg.sender, withdrawAmount, newBalance);
+        return newBalance;
     }
 }
